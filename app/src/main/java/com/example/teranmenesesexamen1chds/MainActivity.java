6d720f837b512a99ae8aa222ff844ca760ee396e
Kevin750xd <kevinteran750@gmail.com>
2024-12-09 10:07:38 -0500

    Cliente Funcionando Mediante IP

--- app/src/main/java/com/example/teranmenesesexamen1chds/MainActivity.java
+++ app/src/main/java/com/example/teranmenesesexamen1chds/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.teranmenesesexamen1chds;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtRespuesta;
+    private Button btnAceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtRespuesta = findViewById(R.id.txtRespuesta);
+        btnAceptar = findViewById(R.id.btnAcceptar);
+
+        btnAceptar.setOnClickListener(v -> obtenerMensaje());
+    }
+
+    private void obtenerMensaje() {
+        String url = "http://169.254.132.108:3000/teran";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        txtRespuesta.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        txtRespuesta.setText("Error: " + error.getMessage());
+                    }
+                });
+        queue.add(stringRequest);
     }
 }
